cdd618c56d57ce0ed0b30c79b07bfc7cbfa81abd
Fide1212 <167130175+Fide1212@users.noreply.github.com>
2024-04-25 01:38:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['255689980623', 'Fidelisscott', true],
+  ['255689980623', 'Fidelis scott', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
